ce7d292a189e3b63de39e39d9a077ea760048134
CVS CHARAN <charan.cvs@gmail.com>
2025-06-18 13:00:19 +0530

    bug-fix

--- src/pages/Webinar.tsx
+++ src/pages/Webinar.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Webinar = () => {
-  return (
-    <div>Webinar</div>
-  )
-}
+  return <div>Webinar</div>;
+};
 
-export default Webinar
+export default Webinar;
--- src/pages/Webniar.tsx
+++ src/pages/Webniar.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Webniar = () => {
-  return (
-    <div>Webniar</div>
-  )
-}
+  return <div>Webniar</div>;
+};
 
-export default Webniar
+export default Webniar;
